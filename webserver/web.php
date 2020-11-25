30b4cebdcd4003d5e1cb62e5246677c4c233e7ca
Rubik <dinhtham18032000@gmail.com>
2020-11-25 20:34:03 +0700

    thiet ke giao dien web

--- webserver/web.php
+++ webserver/web.php
@@ -14,7 +14,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.0.5/css/adminlte.min.css">
 </head>
-<header>
+<header >
+
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
@@ -45,37 +46,149 @@
             <span class="sr-only">Next</span>
         </a>
     </div>
-    <nav class="navbar navbar-expand-lg navbar-ligh bg-li" >
+<!--    class="fixed-top"-->
+<!--    <div class="sticky-top">...</div>-->
+    <div >
+        <nav class="navbar navbar-expand-lg navbar-ligh bg-li " >
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#" ><i class='fas fa-house-user' ></i>  &ensp;|<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="form_upload.php">Upload file &ensp;|</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">List file GLB  &ensp;|</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Show view GLB  &ensp;&ensp;|</a>
-                </li>
+            <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#" ><i class='fas fa-house-user' ></i>  &ensp;|<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="form_upload.php">Upload file &ensp;|</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">List file GLB  &ensp;|</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Show view GLB  &ensp;&ensp;|</a>
+                    </li>
 
-                <li class="nav-item">
-                    <a class="nav-link pd-0 " href="login.php" tabindex="-1" aria-disabled="true">Logout  &ensp;|</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Help</a>
-                </li>
-            </ul>
-            <!-- -->
-            <p class="nav-link" id="day"> Thắm Đinh_18001201 -  <?php echo date("l, M d, Y")?>&emsp;</p>
-        </div>
-    </nav>
+                    <li class="nav-item">
+                        <a class="nav-link pd-0 " href="login.php" tabindex="-1" aria-disabled="true">Logout  &ensp;|</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Help</a>
+                    </li>
+                </ul>
+                <!-- -->
+                <p class="nav-link" id="day"> Thắm Đinh_18001201 -  <?php echo date("l, M d, Y")?>&emsp;</p>
+            </div>
+        </nav>
+    </div>
 </header>
-<body></body>
-<footer></footer>
+<body >
+
+<div class="container">
+    <p>
+        Lúc 23h20 ngày 3/11, Fox News, hãng tin yêu thích của Trump, là kênh truyền thông đầu tiên công bố Biden chiến thắng ở “pháo đài đỏ” Arizona.
+
+        Đây là lần đầu tiên sau 24 năm một ứng viên Dân chủ được tuyên bố chiến thắng tại Arizona, bang có truyền thống ủng hộ đảng Cộng hòa, cũng đồng nghĩa với việc triển vọng tái đắc cử của Trump thu hẹp lại. Do đó, Tổng thống Mỹ và chiến dịch của ông vô cùng giận dữ, thực hiện một loạt cuộc gọi đến Fox News để yêu cầu hãng tin này rút lại tuyên bố.
+
+        Dựa trên phân tích dữ liệu của mình và số phiếu đã được kiểm, Fox News vẫn kiên định với dự báo Biden thắng ở Arizona và kiên quyết không rút lại thông tin như yêu cầu của Trump. Hãng tin AP cũng có dự báo tương tự, trong khi một loạt hãng tin lớn khác của Mỹ tỏ ra thận trọng, cho rằng cuộc đua ở Arizona quá sít sao nên chưa thể tuyên bố người chiến thắng vào lúc đó.
+
+        Những người ủng hộ Trump cũng tỏ ra bối rối. Trong cuộc biểu tình bên ngoài một trung tâm kiểm phiếu tại thành phố Phoenix, bang Arizona, đám đông hét lớn “Fox News tồi tệ!”, trút cơn thịnh nộ lên kênh tin tức đã dành 4 năm qua để ca ngợi hầu hết động thái và phát biểu của Trump.
+
+        Hai tuần sau khi tuyên bố Biden chiến thắng ở Arizona, Fox News, kênh truyền hình cáp nhiều người xem nhất ở Mỹ, giờ đây mắc kẹt giữa những tranh cãi về cuộc bầu cử tổng thống Mỹ và cơn thịnh nộ chưa nguôi của Trump. Tuần trước, Tổng thống đăng một loạt tweet chỉ trích Fox News, lên án kênh truyền hình này “vong ân bội nghĩa”.
+    </p>
+    <p>
+        “Tỷ lệ xem vào ban ngày của Fox News đã hoàn toàn sụp đổ. Tỷ lệ xem vào cuối tuần thậm chí tệ hơn. Rất buồn khi thấy điều này xảy ra, nhưng họ đã quên mất những gì khiến họ thành công, những gì đưa họ lên vị trí đó. Họ đã quên mất con gà đẻ trứng vàng. Sự khác biệt lớn nhất giữa cuộc bầu cử 2016 và 2020 chính là Fox News!”, Trump viết.
+
+        Eric Deggans, nhà phân tích truyền thông của NBC News, nhận định Fox News luôn phải bước đi trên ranh giới mong manh giữa việc xây dựng hình ảnh một hãng tin độc lập và thể hiện sự ủng hộ chính trị bảo thủ. “Có những lúc họ hành động như một hãng tin thực sự nhằm duy trì hình ảnh một kênh tin tức độc lập”, Deggans giải thích.
+
+        Tuy nhiên, Fox News bị đánh giá không thể hiện được sự cân bằng một cách khéo léo và liền mạch, với những thay đổi “chóng mặt” giữa các chương trình trên kênh. Vào ban ngày, các quy tắc báo chí chính thống được áp dụng, nhưng tới ban đêm, những người dẫn chương trình của kênh này lại vứt bỏ hết quy tắc đó.
+
+        Hôm 9/11, người dẫn chương trình Neil Cavuto cắt sóng buổi họp báo do thư ký báo chí Nhà Trắng Kayleigh McEnany tổ chức ở trụ sở Ủy ban Quốc gia đảng Cộng hòa, sau khi cô nêu một loạt tuyên bố về gian lận bầu cử mà không đưa bằng chứng. “Tôi không thể để mọi người tiếp tục xem điều này”, Cavuto nói với khán giả.
+
+        Tuy nhiên, ngay tối đó, Sean Hannity và Tucker Carlson, hai người dẫn chương trình nổi tiếng nhất của Fox News, lại đưa khán giả vào một “vũ trụ khác”, khi cáo buộc phe Dân chủ ở bang này “ăn bớt” phiếu bầu, còn ở bang khác thì thổi phồng số phiếu. Trong “vũ trụ” đó của Hannity và Carlson, Trump đã giành chiến thắng trong cuộc bầu cử.
+
+        Tình huống “tréo nghoe” này dường như cho thấy nỗ lực cân bằng tin tức của Fox News ngày càng gặp khó khăn.
+
+        “Hiện nay, rất nhiều người xem Fox News cảm thấy bất cứ điều gì mâu thuẫn với thế giới quan ủng hộ phe bảo thủ đều là sai trái. Tôi nghĩ Fox News sẽ ngày càng loay hoay trong việc duy trì thế cân bằng tin tức”, Deggans nhận định.
+
+        Các chương trình thu hút người xem nhất trên Fox News đều thuộc về Carlson và Hannity. Trong quý II năm nay, chương trình Tucker Carlson Tonight lập kỷ lục với 4,331 triệu người xem, còn chương trình của Hannity đứng thứ hai với 4,311 triệu. Do đó, câu hỏi được đặt ra là tại sao Fox News không đưa tin một cách nhất quán theo hướng ủng hộ phe bảo thủ và hậu thuẫn Trump.
+
+        Angelo Carusone, chủ tịch kiêm giám đốc điều hành Media Matters, một tổ chức giám sát truyền thông, giải thích rằng sức mạnh của Fox News “rốt cuộc vẫn tới từ quy mô và mức độ gắn bó của khán giả“, với mục tiêu thuyết phục người xem trả tiền truyền hình cáp.
+
+        “Fox News đang phải thuyết phục rất nhiều khán giả tiếp tục ký hợp đồng truyền hình cáp với họ. Nếu quá thiên về cánh hữu, họ có thể bị coi là một kênh tin tức cực đoan, phiến diện và không đáp ứng đủ các tiêu chuẩn của báo chí, ảnh hưởng đến khả năng đàm phán hiệu quả”, Carusone cho hay.
+
+        Tuy nhiên, việc duy trì các nguyên tắc báo chí lại châm ngòi cho sự bất bình của những người ủng hộ Trump, khiến Fox News rơi vào thế “tiến thoái lưỡng nan”. “Đã đến lúc chuyển sang xem Newsmax hoặc One America News (OAN). Fox News đã chính thức gia nhập mạng lưới truyền thông mục nát”, một người dùng Facebook viết. Đây chỉ là một trong hàng loạt bài đăng kêu gọi tẩy chay Fox News.
+
+        Fox News từng vượt qua những làn sóng chỉ trích trước đây. Tuy nhiên, điểm khác biệt là hiện nay có những kênh tin tức bảo thủ mới cạnh tranh với họ. Theo Carusone, Newsmax và OAN thực sự có thể hút một lượng khán giả nhất định của Fox News.
+
+        Hai kênh này đều thiên hữu hơn so với Fox News, đưa tin về những thuyết âm mưu kỳ lạ như sự tồn tại của một “nhà nước ngầm”, âm mưu của đảng Dân chủ nhằm truyền nCoV cho Trump, hay cố vấn y tế Nhà Trắng Anthony Fauci đã tài trợ cho việc tạo ra nCoV.
+
+        Dữ liệu của CNN cho thấy Newsmax TV hồi mùa hè thu hút khoảng 25.000 người xem mỗi ngày. Trong tuần bầu cử, con số này tăng lên 182.000. Tuy nhiên, sự gia tăng mạnh mẽ thực sự diễn ra sau khi Fox News hứng một loạt chỉ trích, với các chương trình buổi tối của Newsmax thu hút 700.000 – 800.000 người xem.
+
+        Fox News lớn mạnh hơn nhiều so với hai kênh mới nổi trên. Tuy nhiên, tác động từ phía Trump có thể khiến lượng người xem Newsmax tiếp tục tăng lên nhanh chóng. Hôm 12/11, Trump tweet lại 5 bài đăng ca ngợi Newsmax. Hicks Equity Partners, công ty đầu tư có quan hệ với Ủy ban Quốc gia đảng Cộng hòa, thậm chí đang đàm phán mua lại Newsmax để biến kênh này thành đối thủ cạnh tranh với Fox News.
+
+        Heather Hendershot, giáo sư truyền thông tại Viện Công nghệ Massachusetts, suy đoán Trump thậm chí có thể phát triển mạng lưới truyền hình của riêng mình, nhằm phục vụ cánh hữu cũng như những người yêu mến ông. “Đây vốn luôn là mục tiêu của Trump”, Hendershot nói. Tuy nhiên, điều này dường như ít có khả năng xảy ra khi Trump được cho là đang đối mặt với các khoản nợ và những cuộc chiến pháp lý sau khi rời ghế tổng thống.
+
+        Hendershot nói thêm rằng Fox News rõ ràng vẫn sẽ tồn tại. Sự xuất hiện của một tổng thống Dân chủ thậm chí có thể trao cho kênh truyền hình này cơ hội trở lại phát huy thế mạnh của mình.
+
+        “Việc Biden ở trong Nhà Trắng có khả năng giúp ích cho Fox News, khi hai bên đối lập nhau. Fox News hoạt động rất tốt dưới thời Obama, bởi họ có đối tượng rõ ràng để chống lại”, giáo sư cho hay.
+
+        Ánh Ngọc (Theo Guardian)
+    </p>
+</div>
+<footer class="footer-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-3">
+                <div class="footer-single-widget">
+                    <a href="javascript:void(0);">
+                        <img style="padding-bottom: 20px;" src="/php-intership-team/img/main/logofooter.png"></a>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.9187581313945!2d105.80576221493206!3d20.995893486016204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135acbf0df2c0e5%3A0xd740a66998e1a0ed!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBLaG9hIGjhu41jIFThu7Egbmhpw6puIChWTlUgVW5pdmVyc2l0eSBvZiBTY2llbmNlKQ!5e0!3m2!1svi!2s!4v1597976299929!5m2!1svi!2s" width="210" height="64" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 section-padding-0">
+                <h3>TRƯỜNG ĐẠI HỌC KHOA HỌC TỰ NHIÊN</h3>
+                <p><strong>ĐỊA CHỈ</strong>: 334 Nguyễn Trãi - Thanh Xuân - Hà Nội</p>
+                <p><strong>Điện thoại</strong>: (84) 0243-8584615 / 8581419</p>
+                <p><strong>Fax</strong>: (84) 0243-8523061</p>
+                <p><strong>Email</strong>: hus@vnu.edu.vn - admin@hus.edu.vn</p>
+            </div>
+            <div class="col-12 col-md-3">
+                <h3>THÔNG TIN HỮU ÍCH</h3>
+                <div class="footer-single-widget">
+                    <ul class="footer-menu d-flex justify-content-between">
+                        <li><a href="http://vnu.edu.vn/home/" target="_blank">Website VNU</a></li>
+                        <li><a href="/php-intership-team/frontend/Tailieubieumau/tailieubieumau.php">Tài liệu biểu mẫu</a></li>
+                        <li><a href="http://mail.vnu.edu.vn/" target="_blank">Thư điện tử VNU</a></li>
+                        <li><a href="http://data.hus.vnu.edu.vn/" target="_blank">Tài nguyên số</a></li>
+                        <li><a href="http://mail.hus.edu.vn/" target="_blank">Thư điện tử HUS</a></li>
+                        <li><a href="javascript:void(0);">Liên kết Website</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-2 pd-0">
+                <h3>Mạng xã hội</h3>
+                <p class="social">
+                    <a href="https://www.facebook.com/HusFanpage/" target="_blank"><i class="fab fa-facebook-f"></i>Fanpage Trường ĐHKHTN</
+                </p>
+                <p class="social">
+                    <a href="https://www.facebook.com/VNUHUSFanpage/" target="_blank"> <i class="fab fa-facebook-f"></i> Fanpage Tư vấn tuyển sinh</a>
+                </p>
+
+            </div>
+        </div>
+    </div>
+    <div class="copywrite">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 co-md-10">
+                    <p>Bản quyền © 2020 Trường ĐHKHTN-ĐHQGHN</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+
+
 </html>
 
 
