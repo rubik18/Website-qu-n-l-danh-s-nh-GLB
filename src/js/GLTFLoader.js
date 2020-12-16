58a3158c07571fac932175f0642fd4f0a822d65c
kaioz11 <hoangmai.tq2000@gmail.com>
2020-12-16 15:44:43 +0700

    add GUI

--- src/js/GLTFLoader.js
+++ src/js/GLTFLoader.js
@@ -1,9 +1,19 @@
 import *as THREE from '../three/three.module.js'
 import {GLTFLoader} from '../three/GLTFLoader.js'
-import {OrbitControls} from '../three/OrbitControls.js'
+import {OrbitControls} from '../three/OrbitControls.js';
+import {GUI} from '../three/dat.gui.module.js';
 
 var camera, scene, renderer;
 var mixer, clock;
+
+let ambientLight, light;
+
+let effectController;
+
+let wireMaterial, flatMaterial, gouraudMaterial;
+
+const diffuseColor = new THREE.Color();
+const specularColor = new THREE.Color();
 
 function init() {
     scene = new THREE.Scene();
@@ -16,12 +26,26 @@ function init() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    var light = new THREE.AmbientLight( 0x404040 ); // soft white light
-    scene.add( light );
+    ambientLight  = new THREE.DirectionalLight( 0xefefff, 1.5 );
+    ambientLight.position.set( 1, 1, 1 ).normalize();
+    scene.add(ambientLight)
+
+    light = new THREE.DirectionalLight( 0xffefef, 1.5 );
+    light.position.set( - 1, - 1, - 1 ).normalize();
+    scene.add(light);
 
     var controls = new OrbitControls( camera, renderer.domElement);
 
     clock = new THREE.Clock();
+
+    const materialColor = new THREE.Color();
+    materialColor.setRGB( 1.0, 1.0, 1.0 );
+    
+    wireMaterial = new THREE.MeshBasicMaterial( { color: 0xFFFFFF, wireframe: true } );
+
+    flatMaterial = new THREE.MeshPhongMaterial( { color: materialColor, specular: 0x000000, flatShading: true, side: THREE.DoubleSide } );
+
+    gouraudMaterial = new THREE.MeshLambertMaterial( { color: materialColor, side: THREE.DoubleSide } );
 
     var textureLoader = new THREE.TextureLoader();
     var text = textureLoader.load('../../data/12.jpg');
@@ -29,10 +53,10 @@ function init() {
     text.flipY = false;
 
     var loader = new GLTFLoader();
-    loader.load('../../data/test1.1.glb', (gltf) => {
+    loader.load('../../data/Horse.glb', (gltf) => {
         var obj = gltf.scene;
-        // obj.scale.set(0.02, 0.02, 0.02);
-        obj.scale.set(0.5, 0.5, 0.5)
+        obj.scale.set(0.02, 0.02, 0.02);
+        // obj.scale.set(0.5, 0.5, 0.5)
 
         obj.traverse((o) => {
             if(o.isMesh) {
@@ -40,8 +64,8 @@ function init() {
             }
         })
 
-        console.log(gltf)
-        console.log(obj)
+        setupGui();
+
 
         mixer = new THREE.AnimationMixer( obj );
         mixer.clipAction( gltf.animations[ 0 ] ).play();
@@ -50,6 +74,64 @@ function init() {
     })
 
     window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function setupGui() {
+
+    effectController = {
+
+        ka: 0.17,
+        kd: 0.51,
+        ks: 0.2,
+
+        hue:	0.121,
+        saturation: 0.73,
+        lightness: 0.66,
+
+        lhue:	0.04,
+        lsaturation: 0.01,
+        llightness: 1.0,
+
+        lx: 0.32,
+        ly: 0.39,
+        lz: 0.7,
+        newTess: 15,
+        bottom: true,
+        lid: true,
+        body: true,
+        fitLid: false,
+        nonblinn: false,
+    };
+
+    let h;
+
+    const gui = new GUI();
+
+    // material (color)
+
+    h = gui.addFolder( "Material color" );
+
+    h.add( effectController, "hue", 0.0, 1.0, 0.025 ).name( "hue" ).onChange( render );
+    h.add( effectController, "saturation", 0.0, 1.0, 0.025 ).name( "saturation" ).onChange( render );
+    h.add( effectController, "lightness", 0.0, 1.0, 0.025 ).name( "lightness" ).onChange( render );
+
+    // light (point)
+
+    h = gui.addFolder( "Lighting" );
+
+    h.add( effectController, "lhue", 0.0, 1.0, 0.025 ).name( "hue" ).onChange( render );
+    h.add( effectController, "lsaturation", 0.0, 1.0, 0.025 ).name( "saturation" ).onChange( render );
+    h.add( effectController, "llightness", 0.0, 1.0, 0.025 ).name( "lightness" ).onChange( render );
+    h.add( effectController, "ka", 0.0, 1.0, 0.025 ).name( "ambient" ).onChange( render );
+
+    // light (directional)
+
+    h = gui.addFolder( "Light direction" );
+
+    h.add( effectController, "lx", - 1.0, 1.0, 0.025 ).name( "x" ).onChange( render );
+    h.add( effectController, "ly", - 1.0, 1.0, 0.025 ).name( "y" ).onChange( render );
+    h.add( effectController, "lz", - 1.0, 1.0, 0.025 ).name( "z" ).onChange( render );
+
 }
 
 function onWindowResize() {
@@ -61,6 +143,25 @@ function onWindowResize() {
     animate();
 }
 
+function render() {
+    
+    // diffuseColor.multiplyScalar( effectController.kd );
+    flatMaterial.color.copy( diffuseColor );
+    gouraudMaterial.color.copy( diffuseColor );
+    
+
+    // specularColor.multiplyScalar( effectController.ks );
+
+    // Ambient's actually controlled by the light for this demo
+    ambientLight.color.setHSL( effectController.hue, effectController.saturation, effectController.lightness * effectController.ka );
+
+    light.position.set( effectController.lx, effectController.ly, effectController.lz );
+    light.color.setHSL( effectController.lhue, effectController.lsaturation, effectController.llightness );
+
+
+    renderer.render(scene, camera);
+}
+
 function animate() {
     requestAnimationFrame( animate );
 
@@ -68,7 +169,7 @@ function animate() {
         mixer.update( clock.getDelta() );
     }
 
-    renderer.render( scene, camera );
+    render();
 }
 
 init()
